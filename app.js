0a6e979b4dc8564b6fc341f81b9a83102cdf11a9
Kevin Chen <chen.kcw@gmail.com>
2021-10-04 10:00:58 -0700

    Add partials and main page

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 const PORT = 5000;
 const DOMAIN = 'localhost';
